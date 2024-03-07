b3edd4b9df40891d816346cafaa4609f6d04ab72
Andika Eka Putra <andika.eka.102@gmail.com>
2024-03-07 20:28:25 +0700

    Initial commit

---
+++ app/src/main/java/com/anekra/aerport/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.anekra.aerport.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue = Color(0xFF73ACCA)
+val BlueGrey = Color(0xFFA2BCCB)
+val Green = Color(0xFF00ccaa)
+
+val DarkBlue = Color(0xFF002A60)
+val DarkBlueGrey = Color(0xFF5A7274)
+val DarkGreen = Color(0xFF172921)
