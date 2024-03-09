f03732e0612db048306e24c532e40969769aba61
Andika Eka Putra <andika.eka.102@gmail.com>
2024-03-09 20:52:27 +0700

    Feature: add Constants object

---
+++ app/src/main/java/com/anekra/aerport/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.anekra.aerport.utils
+
+object Constants {
+    const val HOME_ROUTE = "homeRoute"
+    const val PROJECT_ROUTE = "projectRoute"
+    const val BLOG_ROUTE = "blogRoute"
+    const val CONTACT_ROUTE = "contactRoute"
+}
