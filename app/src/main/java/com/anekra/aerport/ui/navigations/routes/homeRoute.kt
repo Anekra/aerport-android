0ff535e8ce8f2854d00da6b24d2f28ff9d0704ee
Andika Eka Putra <andika.eka.102@gmail.com>
2024-03-14 20:53:54 +0700

    Refactor: add navHostController as HomeScreen parameter

--- app/src/main/java/com/anekra/aerport/ui/navigations/routes/homeRoute.kt
+++ app/src/main/java/com/anekra/aerport/ui/navigations/routes/homeRoute.kt
@@ -1,12 +1,13 @@
 package com.anekra.aerport.ui.navigations.routes
 
+import androidx.compose.foundation.ScrollState
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import com.anekra.aerport.ui.screens.HomeScreen
 
-fun NavGraphBuilder.homeRoute(navHostController: NavHostController) {
+fun NavGraphBuilder.homeRoute(navHostController: NavHostController, verticalScroll: ScrollState) {
     composable(route = Routes.Home.route) {
-        HomeScreen()
+        HomeScreen(navHostController = navHostController, verticalScroll = verticalScroll)
     }
 }
